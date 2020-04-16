5cab56ad291b560f119b33e9f4c5c24930b229de
mary-abigail <maryabigail0902@gmail.com>
2020-04-15 23:26:37 -0700

    attempt at connecting activities (cont.)

--- app/src/main/java/com/nyit/employee_scheduler/MainActivity.java
+++ app/src/main/java/com/nyit/employee_scheduler/MainActivity.java
@@ -2,7 +2,11 @@ package com.nyit.employee_scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.nyit.employee_scheduler.ui.login.LoginActivity;
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void managerLogIn(View v){    // Log In as Manager/Admin
+        Intent loginToManager = new Intent(getContext(), LoginActivity.java);
+        startActivity(loginToManager);
+    }
+
+    public void employeeLogIn(View v){   //Log In as Employee
+        Intent loginToEmployee = new Intent(getContext(), LoginActivity.java);
+        startActivity(loginToEmployee);
+
     }
 }
