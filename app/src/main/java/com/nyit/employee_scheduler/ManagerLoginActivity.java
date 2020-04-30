f98fcee6af51fbedf1a05065acc980354290f49d
mary-abigail <maryabigail0902@gmail.com>
2020-04-29 22:29:23 -0700

    Manager pages

---
+++ app/src/main/java/com/nyit/employee_scheduler/ManagerLoginActivity.java
@@ -0,0 +1,97 @@
+package com.nyit.employee_scheduler;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ManagerLoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        configureLogin();
+    }
+
+    private void configureLogin(){
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendLoginRequest(username.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+
+
+    private void displayErrorToast(){}
+
+    private void sendLoginRequest(final String username, final String password){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://64.190.90.187/api/user/authenticate.php");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("Username", username );
+                    jsonParam.put("Password", password);
+
+
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    if(String.valueOf(conn.getResponseCode()).equals("200")){
+                        Intent intent = new Intent(ManagerLoginActivity.this, ManagerDashboardActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        ManagerLoginActivity.this.username.setText("");
+                        ManagerLoginActivity.this.password.setText("");
+                        ManagerLoginActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(ManagerLoginActivity.this,"Invalid username or password",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
