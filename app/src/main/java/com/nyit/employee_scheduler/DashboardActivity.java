1ac80b04dc6ed68f15379a7bbba1f70e00cbce8c
mary-abigail <maryabigail0902@gmail.com>
2020-05-02 13:37:26 -0700

    stuff and things

--- app/src/main/java/com/nyit/employee_scheduler/DashboardActivity.java
+++ app/src/main/java/com/nyit/employee_scheduler/DashboardActivity.java
@@ -39,7 +39,7 @@ public class DashboardActivity extends AppCompatActivity {
         notificationCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, Notification.class);
+                Intent intent = new Intent(DashboardActivity.this, NotificationActivity.class);
                 startActivity(intent);
             }
         });
