647ca54ec05e2f44ac200e824799e5c68c7cb99d
Destiny Stribling <35977902+dstribli@users.noreply.github.com>
2020-05-02 16:36:09 -0400

    everything

--- app/src/main/java/com/nyit/employee_scheduler/DaysOffRequest.java
+++ app/src/main/java/com/nyit/employee_scheduler/DaysOffRequest.java
@@ -1,13 +1,12 @@
 package com.nyit.employee_scheduler;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 
@@ -17,76 +16,79 @@ import java.net.URL;
 
 public class DaysOffRequest extends AppCompatActivity {
 
-    private EditText fromDay;
-    private EditText toDay;
-    private Button submitButton;
+        private Button submitDaysOffButton;
+        private TextView desiredDateInput;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_daysoff_request);
-        fromDay = findViewById(R.id.fromDay);
-        toDay = findViewById(R.id.toDay);
-        submitButton = findViewById(R.id.switchSubmitButton);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_daysoff_request);
+            submitDaysOffButton = findViewById(R.id.submitDaysOffButton);
+            desiredDateInput = findViewById(R.id.desiredDateInput);
+            configureRequests();
 
-        configureRequests();
 
-    }
-//Days Off Request is creating a request to add a specific time to have days off based on vacation time and current schedule of employee (and/or other employees)
-    private void configureRequests(){
+        }
+        //Days Off Request is creating a request to add a specific time to have days off based on vacation time and current schedule of employee (and/or other employees)
+        private void configureRequests(){
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                daysOffRequest(fromDay.getText().toString(), toDay.getText().toString());
-            }
-        });
-    }
-
-    private void daysOffRequest(final String fromDay, final String toDay) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {//request method might be changed depending on API
-                    URL url = new URL("API FOR SCHEDULE");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept", "application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-
-                    JSONObject jsonParam = new JSONObject();
-                    jsonParam.put("From Day", fromDay );
-                    jsonParam.put("From Day", toDay);
-
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    os.writeBytes(jsonParam.toString());
-
-                    os.flush();
-                    os.close();
-
-                    if (String.valueOf(conn.getResponseCode()).equals("whatever the response code ends up being")) {
-                        //SUBMIT FROM DAY AND TO DAY DATES
-                        Intent intent = new Intent( DaysOffRequest.this, RequestsActivity.class);
-                        startActivity(intent);
-                    } else {
-                        DaysOffRequest.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(DaysOffRequest.this, "Request was not submitted, please try again. ", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                    }
-
-                    conn.disconnect();
-                } catch (Exception e) {
-                    e.printStackTrace();
+            submitDaysOffButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    daysOffRequest(desiredDateInput.getText().toString());
                 }
-            }
-        });
+            });
+        }
 
-        thread.start();
+        private void daysOffRequest(final String desiredDateInput) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {//request method might be changed depending on API
+                        URL url = new URL("http://64.190.90.187/api/request/createRequest.php");
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setRequestMethod("POST");
+                        conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                        conn.setRequestProperty("Accept", "application/json");
+                        conn.setDoOutput(true);
+                        conn.setDoInput(true);
+
+                        JSONObject jsonParam = new JSONObject();
+                        jsonParam.put("DateRequested", "2020-05-02");
+                        jsonParam.put("EmployeeID", "1234556");
+                        jsonParam.put("Status", "unapproved");
+                        jsonParam.put("RequestType","vacation");
+                        jsonParam.put("Message","hello!");
+                        jsonParam.put("RequestID","12345");
+
+
+                        DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                        os.writeBytes(jsonParam.toString());
+
+                        os.flush();
+                        os.close();
+
+                        if (String.valueOf(conn.getResponseCode()).equals("200")) {
+                            finish();
+                        } else {
+                            DaysOffRequest.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(DaysOffRequest.this, "Request was not submitted, please try again. ", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+
+                        conn.disconnect();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
+        }
     }
-}
+
+
