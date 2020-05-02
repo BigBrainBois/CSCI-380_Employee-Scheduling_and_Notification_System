a23c4ecf34c1d39dd8e601a419d9b6c10f21b9ca
mary-abigail <maryabigail0902@gmail.com>
2020-05-02 14:07:54 -0700

    time dialog

--- app/src/main/java/com/nyit/employee_scheduler/SwitchSchedule.java
+++ app/src/main/java/com/nyit/employee_scheduler/SwitchSchedule.java
@@ -2,13 +2,23 @@ package com.nyit.employee_scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SwitchSchedule extends AppCompatActivity {
+
+    private TimePickerDialog timeDialogue;
+    private Button toButton;
+    private Button fromButton;
+    private Button toTime;
+    private Button fromTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switch_schedule);
+        toButton =
+
     }
 }
